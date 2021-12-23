421829e828d36a42f4a344487b8e2e18805ccd10
Dennis Periquet <dperique@redhat.com>
2021-12-23 12:14:05 -0500

    create TestRuns table

---
+++ pkg/jobrunaggregator/jobrunaggregatorapi/types_row_testrun.go
@@ -0,0 +1,41 @@
+package jobrunaggregatorapi
+
+// The jobSchema below is used to build the "Jobs" table.
+//
+const (
+	TestRunTableName = "TestRuns"
+	TestRunSchema    = `
+[
+  {
+    "mode": "REQUIRED",
+    "name": "Name",
+    "description" : "Name of the test run"
+    "type": "STRING"
+  },
+  {
+    "mode": "REQUIRED",
+    "name": "JobRunName",
+    "description" : "Name of the JobRun (big number) that ran this test (e.g., 1389486541524439040)"
+    "type": "STRING"
+  },
+  {
+    "mode": "REQUIRED",
+    "name": "JobName",
+    "description" : "Name of the Job that as this test in it"
+    "type": "STRING"
+  },
+  {
+    "mode": "REQUIRED",
+    "name": "Status",
+    "description" : "Status of the test (e.g., pass, fail)"
+    "type": "STRING"
+  }
+  {
+    "mode": "NULLABLE",
+    "name": "TestSuite",
+    "description" : "Testsuite that this test belongs to"
+    "type": "STRING"
+  }
+]
+`
+)
