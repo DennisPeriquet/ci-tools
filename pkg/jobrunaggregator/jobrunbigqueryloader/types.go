8a14e863de997b29ed896d43946589edf20361e4
Dennis Periquet <dperique@redhat.com>
2021-12-23 15:32:52 -0500

    cast to see values

--- pkg/jobrunaggregator/jobrunbigqueryloader/types.go
+++ pkg/jobrunaggregator/jobrunbigqueryloader/types.go
@@ -46,8 +46,9 @@ func (d dryRunInserter) Put(ctx context.Context, src interface{}) (err error) {
 	buf := &bytes.Buffer{}
 	fmt.Fprintf(buf, "BULK INSERT into %v\n", d.table)
 	for i := 0; i < srcVal.Len(); i++ {
-		s := srcVal.Index(i).Interface()
-		fmt.Fprintf(buf, "\tINSERT into %v: %+v\n", d.table, s.(*jobrunaggregatorapi.JobRunRow))
+		s := srcVal.Index(i).Interface().(*testRunRow)
+		fmt.Fprintf(buf, "\tINSERT into %v: prowJob=%v, ts=%v lts=%v tcn=%v\n", d.table, s.prowJob.ObjectMeta.Name,
+			s.testSuites[0], len(s.testSuites), s.testCase.Name)
 	}
 	fmt.Fprint(d.out, buf.String())
 
