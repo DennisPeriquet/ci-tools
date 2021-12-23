339600589665ab7cf0163add6c60d9bd0ed610bf
Dennis Periquet <dperique@redhat.com>
2021-12-23 12:28:35 -0500

    fix missing commas in TestRun schema json

--- pkg/jobrunaggregator/jobrunaggregatorapi/types_row_testrun.go
+++ pkg/jobrunaggregator/jobrunaggregatorapi/types_row_testrun.go
@@ -9,31 +9,31 @@ const (
   {
     "mode": "REQUIRED",
     "name": "Name",
-    "description" : "Name of the test run"
+    "description" : "Name of the test run",
     "type": "STRING"
   },
   {
     "mode": "REQUIRED",
     "name": "JobRunName",
-    "description" : "Name of the JobRun (big number) that ran this test (e.g., 1389486541524439040)"
+    "description" : "Name of the JobRun (big number) that ran this test (e.g., 1389486541524439040)",
     "type": "STRING"
   },
   {
     "mode": "REQUIRED",
     "name": "JobName",
-    "description" : "Name of the Job that as this test in it"
+    "description" : "Name of the Job that as this test in it",
     "type": "STRING"
   },
   {
     "mode": "REQUIRED",
     "name": "Status",
-    "description" : "Status of the test (e.g., pass, fail)"
+    "description" : "Status of the test (e.g., pass, fail)",
     "type": "STRING"
   },
   {
     "mode": "NULLABLE",
     "name": "TestSuite",
-    "description" : "Testsuite that this test belongs to"
+    "description" : "Testsuite that this test belongs to",
     "type": "STRING"
   }
 ]
